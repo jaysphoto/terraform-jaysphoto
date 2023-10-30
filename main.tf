9371d236f4b4f9ac734789a0e0abd75a27810348
jaysphoto <mail@jaysphoto.me>
2023-10-30 15:16:15 +0100

    Initial commit Terraform project

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
